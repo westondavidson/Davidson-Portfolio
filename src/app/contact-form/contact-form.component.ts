06dbddcbbd833ab7380a84d22fccdc1d7452158c
Weston Davidson <weston@westondavidson.com>
2020-04-06 12:34:17 -0400

    Removed some clutter code from the contact form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -7,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
-googleRedirect = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdkBbR0GzMhR4jNeTDXWBm0c0iaC9tlQOnQJvi3be4YJ72Ikw/formResponse";
-
 closeWin(){
   this.googleRedirect.close();
 
